2fc9d19c5e24dd63783a15e88905831c31b995a5
XD.Wang <xiandui.wxd@alibabapictures.com>
2018-06-21 16:54:12 +0800

    add example

--- src/main/groovy/com/lucifiere/core/handler/AcreageHandler.groovy
+++ src/main/groovy/com/lucifiere/core/handler/AcreageHandler.groovy
@@ -1,5 +1,6 @@
 package com.lucifiere.core.handler
 
+import com.lucifiere.entity.bo.BuildingReport
 import com.lucifiere.entity.vo.HousingEstate
 import com.lucifiere.entity.vo.HousingEstateHandleReq
 import com.lucifiere.entity.vo.HousingEstateHandleResp
@@ -9,6 +10,16 @@ class AcreageHandler extends BaseHouseEstateHandler {
     @Override
     void doBizLogic(HousingEstateHandleReq req, HousingEstateHandleResp resp) {
         List<HousingEstate> housingEstates = req.getHousingEstates()
+        List<BuildingReport> buildingReports = []
+        housingEstates.each {
+            it.buildings.each {
+                List<Float> allKinds = it.households.stream().mapToDouble{it.getAcreage()}.distinct().collect()
+                buildingReports << new BuildingReport(){{
+                    setAcreage(allKinds)
+                    setNo(it.getNo())
+                }}
+            }
+        }
     }
 
 }
