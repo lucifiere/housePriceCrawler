1b8f375ffaecae8800aec52d4c4fbc4f5ee152f2
liucong <liucong@kaolafm.com>
2018-06-21 19:15:08 +0800

    implment TotalPriceHander

--- src/main/groovy/com/lucifiere/core/handler/TotalPriceHandler.groovy
+++ src/main/groovy/com/lucifiere/core/handler/TotalPriceHandler.groovy
@@ -1,14 +1,32 @@
 package com.lucifiere.core.handler
 
-import com.lucifiere.entity.vo.HousingEstate
-import com.lucifiere.entity.vo.HousingEstateHandleReq
-import com.lucifiere.entity.vo.HousingEstateHandleResp
+import com.lucifiere.entity.bo.BuildingReport
+import com.lucifiere.entity.vo.*
 
 class TotalPriceHandler extends BaseHouseEstateHandler {
 
     @Override
     void doBizLogic(HousingEstateHandleReq req, HousingEstateHandleResp resp) {
         List<HousingEstate> housingEstates = req.getHousingEstates()
-    }
+        List<BuildingReport> buildingReports = new ArrayList<>()
 
+        //遍历楼盘
+        for (List<Building> buildingList : housingEstates) {
+            BuildingReport buildingReport = new BuildingReport()
+            //遍历楼栋
+            for (List<Household> householdList : buildingList) {
+                BigDecimal numPrice
+                List<Float> acreageList = new ArrayList()
+
+                for (Household household : householdList) {
+                    numPrice += household.getTotalPrice()
+                    acreageList.add(household.getAcreage())
+                }
+                buildingReport.setAcreage()
+                buildingReport.setTotalPrice(numPrice / householdList.size())
+            }
+            buildingReports.add(buildingReport)
+        }
+        resp.setBuildingReports(buildingReports)
+    }
 }
